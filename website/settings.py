f532f1d2f563b575caa028cb8ee927d7c9ab9f24
Justin Zhai <justinzh0905@gmail.com>
2024-08-11 22:11:00 -0400

    changed debug state

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'justin-zhai.com', 'justin.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['justin-zhai.com', 'justin.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
